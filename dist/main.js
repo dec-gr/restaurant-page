0baddcccaef562f98543447c9a1a985315499f6d
Declan <144496393+dec-gr@users.noreply.github.com>
2023-11-11 16:48:24 +0000

    Create home page with JS instead of html

--- dist/main.js
+++ dist/main.js
@@ -1,3 +1,38 @@
 console.log("Hello World!");
 
 const content = document.getElementById("content");
+
+const main = document.createElement("div");
+main.classList.add("main");
+
+const home = document.createElement("div");
+home.classList.add("home");
+
+const p1 = document.createElement("p");
+p1.textContent = "The ancestoral home of Garlic Bread*";
+
+const p2 = document.createElement("p");
+p2.textContent = "*in Brooklyn";
+p2.classList.add("small");
+
+const imgContainer = document.createElement("div");
+imgContainer.classList.add("img-container");
+
+const img = document.createElement("img");
+img.src = "img/chef.png";
+img.alt = "Image of a chef";
+img.classList.add("chef-img");
+
+imgContainer.appendChild(img);
+
+const p3 = document.createElement("p");
+p3.textContent = "Order online or visit us!";
+
+home.appendChild(p1);
+home.appendChild(p2);
+home.appendChild(imgContainer);
+home.appendChild(p3);
+
+main.appendChild(home);
+
+content.appendChild(main);
