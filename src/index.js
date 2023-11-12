05a54861a68d89876f5071e3f9aae6b3a27932df
Declan <144496393+dec-gr@users.noreply.github.com>
2023-11-12 19:25:39 +0000

    Create nav bar functionality

--- src/index.js
+++ src/index.js
@@ -2,4 +2,31 @@ import loadHome from "./home";
 import loadContact from "./contact";
 import loadMenu from "./menu";
 console.log("Hello World!");
-loadMenu();
+
+const content = document.getElementById("content");
+
+loadHome();
+
+const showContactPage = () => {
+  content.innerHTML = "";
+  loadContact();
+};
+
+const showMenuPage = () => {
+  content.innerHTML = "";
+  loadMenu();
+};
+
+const showHomePage = () => {
+  content.innerHTML = "";
+  loadHome();
+};
+
+const contactBtn = document.getElementById("btn-contact");
+contactBtn.addEventListener("click", showContactPage);
+
+const menuBtn = document.getElementById("btn-menu");
+menuBtn.addEventListener("click", showMenuPage);
+
+const homeBtn = document.getElementById("btn-home");
+homeBtn.addEventListener("click", showHomePage);
